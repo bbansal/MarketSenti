042ddb9d2acc55bfb5f58f261fa60e7d1bab1ad2
bbansal <bbansal@linkedin.com>
2009-12-06 14:38:19 -0800

    fixed DesiPandoraServlet.

--- src/java/com/marketsenti/storage/serializer/StringSerializer.java
+++ src/java/com/marketsenti/storage/serializer/StringSerializer.java
@@ -4,13 +4,11 @@ package com.marketsenti.storage.serializer;
 public class StringSerializer implements BytesSerializer<String>
 {
 
-  @Override
   public String fromBytes(byte[] bytes)
   {
     return new String(bytes);
   }
 
-  @Override
   public byte[] getBytes(String object)
   {
     return object.getBytes();
