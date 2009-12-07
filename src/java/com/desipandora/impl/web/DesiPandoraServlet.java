1b9f955b6983aa2bf4fd086cd1f4ad03eac4ebc4
bbansal <bbansal@linkedin.com>
2009-12-06 17:31:06 -0800

    Added stub on servlet side.

--- src/java/com/desipandora/impl/web/DesiPandoraServlet.java
+++ src/java/com/desipandora/impl/web/DesiPandoraServlet.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.desipandora.api.DesiPandoraService;
 import com.desipandora.youTube.DesiPandoraServiceYouTube;
@@ -24,6 +26,15 @@ public class DesiPandoraServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private DesiPandoraService _service;
+
+	private final static String OPERATION = "operation";
+	private final static String GET_NEW_SESSION_ID = "getNewSessionId";
+	private final static String SEARCH = "search";
+	private final static String FEEDBACK = "feedback";
+	private final static String GET_NEXT_SONGS = "getNextSongs";
+	private final static String KEYWORDS = "keywords";
+	private final static String NUM_SONGS = "numSongs";
+	private final static String USER_ID = "uid";
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
@@ -40,13 +51,105 @@ public class DesiPandoraServlet extends HttpServlet {
 		String sessionId = req.getRequestedSessionId();
 		String pathInfo = req.getPathInfo();
 
-		printRequest(cookies, headers, contextPath, queryString, sessionId,
-				pathInfo);
-		resp.getWriter().write(printRequest(cookies, headers, contextPath, queryString, sessionId, pathInfo));
-		resp.getWriter().flush();
+		String reqString = getRequestAsString(cookies, headers, contextPath,
+				queryString, sessionId, pathInfo);
+
+		int operationCode = getOperation(req.getParameter(OPERATION));
+		if (checkValidOperatonCode(operationCode, reqString, resp)) {
+			logger.debug("handling request:" + reqString);
+			handleOperation(operationCode, req, resp);
+		}
 	}
 
-	private String printRequest(Cookie[] cookies, Enumeration headers,
+	private void handleOperation(int operationCode, HttpServletRequest req,
+			HttpServletResponse resp) throws IOException {
+		try
+		{
+		switch (operationCode) {
+		case 1:
+			handleGetNewSessionId(req,resp);
+			break;
+		case 2:
+			handleSearch(req,resp);
+			break;
+		case 3:
+			handleGetNextSongs(req,resp);
+			break;
+		case 4:
+			handleFeedback(req,resp);
+			break;
+		default:
+			resp.sendError(resp.SC_NOT_IMPLEMENTED,
+					"No recognized operation paramter found.");
+		}
+		}
+		catch (Exception e)
+		{
+			logger.error(e);
+			resp.sendError(resp.SC_NOT_MODIFIED, " failed with exception:" + e);
+		}
+		
+		resp.getWriter().flush();
+		resp.getWriter().close();
+	}
+
+	private void handleFeedback(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void handleGetNextSongs(HttpServletRequest req,
+			HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void handleSearch(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void handleGetNewSessionId(HttpServletRequest req,
+			HttpServletResponse resp) throws JSONException, IOException {
+		String uId = (null == req.getParameter(USER_ID)) ? req.getParameter(USER_ID): "defaultUser";
+		String sessionId =  _service.createSessionId(uId);
+			
+		JSONObject object = new JSONObject();
+		object.put("userSessionId", sessionId);
+		resp.getWriter().print(object.toString());
+	}
+
+	private boolean checkValidOperatonCode(int operationCode, String reqString,
+			HttpServletResponse resp) throws IOException {
+		if (-1 == operationCode) {
+			logger.error("Request recieved w/o any valid operation parameter:"
+					+ reqString);
+			resp.sendError(resp.SC_NOT_IMPLEMENTED,
+					"No recognized operation paramter found.");
+			return false;
+
+		}
+
+		return true;
+	}
+
+	private int getOperation(String parameter) {
+		if (null == parameter) {
+			return -1;
+		} else if (GET_NEW_SESSION_ID.equals(parameter)) {
+			return 1;
+		} else if (SEARCH.equals(parameter)) {
+			return 2;
+		} else if (GET_NEXT_SONGS.equals(parameter)) {
+			return 3;
+		} else if (FEEDBACK.equals(parameter)) {
+			return 4;
+		}
+
+		return -1;
+	}
+
+	private String getRequestAsString(Cookie[] cookies, Enumeration headers,
 			String contextPath, String queryString, String sessionId,
 			String pathInfo) {
 		StringBuilder builder = new StringBuilder();
@@ -58,7 +161,6 @@ public class DesiPandoraServlet extends HttpServlet {
 		builder.append("(" + "sessionId:" + sessionId + ")");
 		builder.append("(" + "pathInfo:" + pathInfo + ")");
 
-		logger.info(builder.toString());
 		return builder.toString();
 	}
 
